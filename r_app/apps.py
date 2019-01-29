4c319d390a262f946a0cf7b9efcd96413f7e99c2
Maciej <m.j.dziegielewski@wp.pl>
2019-01-29 19:15:12 +0100

    Project/app

---
+++ r_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RAppConfig(AppConfig):
+    name = 'r_app'
---
+++ r_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class RestaurantModel(models.Model):
+
+    name = models.CharField(max_length=256)
+    city = models.CharField(max_length=128)
+    street = models.CharField(max_length=256)
+    house_number = models.SmallIntegerField()
+
+
+class FoodModel(models.Model):
+    name  = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    restaurant = models.ForeignKey(RestaurantModel, on_delete=models.CASCADE)
+
